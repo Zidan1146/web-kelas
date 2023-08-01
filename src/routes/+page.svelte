78d3ee1ee9dec1107e7bda0560b99f8121c67458
Zidan1146 <zidanrizky1146@gmail.com>
2023-08-01 20:28:51 +0700

    Form Test 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <form action="?/register" method="post">
   <label for="username">Username:</label>
-  <input type="text" name="username" id="username">
-  <input type="email" name="email" id="email">
-  <input type="password" name="password" id="password">
+  <input type="text" name="username" id="username" placeholder="Insert Username Here">
+  <label for="email">Email:</label>
+  <input type="email" name="email" id="email" placeholder="Insert Email Here">
+  <label for="password">Password:</label>
+  <input type="password" name="password" id="password" placeholder="Insert Password Here">
+  <label for="role-selection">Select a Role</label>
   <select name="role-selection" id="role-selection">
     <option value="nothing" selected>Nothing</option>
     <option value="guest">Guest</option>
